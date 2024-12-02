74600b75fff035cae0e3e869902a940d1dc07f3e
Nguyễn Duy Công <cnd.congnguyenduy@gmail.com>
2024-12-02 23:50:55 +0700

    update

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -8,29 +8,29 @@ import { useEffect } from "react";
 
 const Search = () => {
   // const router = useRouter();
-  const [searchResult, setSearchResult] = React.useState([]);
-  const searchParams = useSearchParams();
-  const searchTerm = searchParams.get("search");
-  useEffect(() => {
-    const search = async () => {
-      try {
-        const searchResult = await movieAPIs.searchMovies(searchTerm);
-        setSearchResult(searchResult.data.results);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    search();
-  }, [searchTerm]);
+  // const [searchResult, setSearchResult] = React.useState([]);
+  // const searchParams = useSearchParams();
+  // const searchTerm = searchParams.get("search");
+  // useEffect(() => {
+  //   const search = async () => {
+  //     try {
+  //       const searchResult = await movieAPIs.searchMovies(searchTerm);
+  //       setSearchResult(searchResult.data.results);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+  //   search();
+  // }, [searchTerm]);
   // console.log(searchResult);
   return (
     <div className="py-[64px]">
       <div className="container mx-auto">
         <div className="grid grid-cols-5 gap-4">
-          {searchResult &&
+          {/* {searchResult &&
             searchResult.map((result, index) => (
               <Card movie={result} key={index} />
-            ))}
+            ))} */}
         </div>
       </div>
     </div>
